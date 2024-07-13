e37df19e0ddf632695a0b12728a7242d957ad641
sen2y <pse6704@sookmyung.ac.kr>
2024-07-13 22:46:11 +0900

    feat: 배포주소 연결

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const restaurantRouter = require("./routes/restaurantRouter.js");
 
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin:
+      "http://ec2-52-79-127-33.ap-northeast-2.compute.amazonaws.com:3000/",
     method: ["GET", "POST", "PATCH", "DELETE"],
   })
 );
