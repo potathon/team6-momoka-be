2673797781eb093e80c0f7abd1c4145ed8969a76
sen2y <pse6704@sookmyung.ac.kr>
2024-07-13 15:38:45 +0900

    feat: 검색결과 API 로직구현

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const port = 3000;
-const restaurantRouter = require('./routes/restaurantRouter.js');
+const port = 4000;
+const restaurantRouter = require("./routes/restaurantRouter.js");
 
 let restaurantData = []; // 데이터를 저장할 전역 변수
 
 // 정적 파일 제공
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // 착한 가게 데이터 가져오기 함수
 const fetchData = async () => {
   try {
     const url =
-      'https://api.odcloud.kr/api/15082982/v1/uddi:6c3db587-f3fb-46ef-97a2-cee7703151ba';
+      "https://api.odcloud.kr/api/15082982/v1/uddi:6c3db587-f3fb-46ef-97a2-cee7703151ba";
     const params = new URLSearchParams({
       page: 1,
       perPage: 293,
-      returnType: 'json',
+      returnType: "json",
       serviceKey:
-        'LXsqgWnfJZdxNwgRq5w/npio1lEDVAAeXpxRfKLASpMNvxXF9VKlikcd8SsMeFZ1/KqsFbiJo7gSO/2oQE9FDw==',
+        "LXsqgWnfJZdxNwgRq5w/npio1lEDVAAeXpxRfKLASpMNvxXF9VKlikcd8SsMeFZ1/KqsFbiJo7gSO/2oQE9FDw==",
     });
 
     const response = await fetch(`${url}?${params.toString()}`, {
       headers: {
-        accept: '*/*',
+        accept: "*/*",
       },
     });
     const apiResponseJson = await response.json();
 
     // '업종' 필드가 '음식점'인 항목만 필터링
     restaurantData = apiResponseJson.data.filter(
-      (store) => store.업종 === '음식점'
+      (store) => store.업종 === "음식점"
     );
-    console.log('데이터가 성공적으로 로드되었습니다.');
+    console.log("데이터가 성공적으로 로드되었습니다.");
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error("Error fetching data:", error);
   }
 };
 
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
 });
 
 // 착한 가게 데이터 엔드포인트
-app.use('/api', restaurantRouter);
+app.use("/api", restaurantRouter);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- routes/restaurantRouter.js
+++ routes/restaurantRouter.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const restaurantController = require('../controllers/restaurantController.js');
+const express = require("express");
+const restaurantController = require("../controllers/restaurantController.js");
 
 const router = express.Router();
 
-router.get('/restaurant', restaurantController.getAllList);
+router.get("/restaurant", restaurantController.getAllList);
+// 검색결과 쿼리스트링
+router.get("/restaurant/search", restaurantController.getSearchList);
 
 module.exports = router;
